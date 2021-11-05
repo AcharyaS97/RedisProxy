c3d328fb311a11a28ddf26fc931412986b297603
Saheel Acharya <saheel97@gmail.com>
2021-11-05 12:59:09 -0400

    Add project files.

---
+++ RedisProxy/Controllers/RedisProxyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisProxy.Controllers {
+
+    public class RedisProxyController {
+        private readonly ILogger<RedisProxyController> _logger;
+
+        private readonly Dictionary<string, object> _localCache;
+
+        public RedisProxyController(ILogger<RedisProxyController> logger) {
+            _logger = logger;
+            _localCache = new Dictionary<string, object>();
+        }
+
+        [HttpGet]
+        public string Get(string key) {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
