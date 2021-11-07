fbba9a066e0bcf4d3755b0cd101b8f5316ce3a41
Saheel Acharya <saheel97@gmail.com>
2021-11-07 11:25:13 -0500

    Create RedisConnection class to encapsulate details around connecting to redis backing store and getting from it

---
+++ RedisProxy/Services/Classes/RedisConnection.cs
@@ -0,0 +1,40 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisProxy.Services {
+
+    public class RedisConnection : ICacheConnection {
+        private static string _configurationOptions;
+
+        public RedisConnection(string configurationOptions) {
+            if (configurationOptions == null) throw new ArgumentNullException("configurationOptions");
+            _configurationOptions = configurationOptions;
+        }
+
+        private static IDatabase Cache {
+            get {
+                return Connection.GetDatabase();
+            }
+        }
+
+        private static readonly Lazy<ConnectionMultiplexer> LazyConnection
+          = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(_configurationOptions));
+
+        public static ConnectionMultiplexer Connection {
+            get {
+                return LazyConnection.Value;
+            }
+        }
+
+        public string GetItem(string key) {
+            var output = Cache.StringGet(key);
+            if (output.HasValue) {
+                return output.ToString();
+            }
+            return null;
+        }
+    }
+}
---
+++ RedisProxy/Services/Interfaces/ICacheConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisProxy.Services {
+
+    public interface ICacheConnection {
+        private static string _configurationOptions;
+
+        public string GetItem(string key);
+    }
+}
